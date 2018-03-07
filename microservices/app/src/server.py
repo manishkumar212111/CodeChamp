af9c1fa08a1bd9b15d92920ea1ddad61299dbad8
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-07 11:01:19 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,11 +1,18 @@
-from src import app
-from flask import render_template
-# from flask import jsonify
 
+from flask import render_template,Flask
+# from flask import jsonify
+app=Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route("/template1")
+def home():
+    return render_template('template1.html')
+
+@app.route("/template2")
+def home():
+    return render_template('template2.html')
 
 # Uncomment to add a new URL at /new
 
