337af22ee9e5eebaf59c5bfda6b80f9453bebe5b
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-07 11:10:32 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -2,15 +2,15 @@
 from flask import render_template,Flask
 # from flask import jsonify
 app=Flask(__name__)
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/template1")
+@app.route('/template1')
 def home():
     return render_template('template1.html')
 
-@app.route("/template2")
+@app.route('/template2')
 def home():
     return render_template('template2.html')
 
