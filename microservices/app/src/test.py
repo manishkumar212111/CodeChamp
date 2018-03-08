cdc17ecd608733901eb2e4dbe70dcd217674d4e1
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-08 22:12:40 +0530

    first commit

--- microservices/app/src/test.py
+++ microservices/app/src/test.py
@@ -1,7 +1,6 @@
 import requests
 import json
-
-# This is the url to which the query is made
+'''# This is the url to which the query is made
 url = "https://data.despairing12.hasura-app.io/v1/query"
 
 # This is the json payload for the query
@@ -27,3 +26,8 @@ resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=he
 # resp.content contains the json response.
 for i in range(0,10):
     print resp.json()[i]['aadhar_no']
+    '''
+aadhar='476894302178'
+list=[657894032172,869520329845,476894302178]
+if int(aadhar) in list:
+    print "yes"
