4488fa306adb9e5aabcc7f4160441675d1e03876
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-09 21:12:41 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -357,4 +357,28 @@ def consumer_login():
         if len(aadhar) !=12:
             return render_template('consumer.html',message="Aadhar number must be of 12 digit")
 
-        
+        url = "https://data.despairing12.hasura-app.io/v1/query"
+
+        # This is the json payload for the query
+        requestPayload = {
+            "type": "select",
+            "args": {
+                "table": "Aadhar",
+                "columns": [],
+                "where": {
+                    "aadhar_no": {
+                        "$eq": "12"
+                    }
+                }
+            }
+        }
+
+        # Setting headers
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer 4f3156a40c12394198aaa87dacd0b53ebf32d1d3ee4271b8"
+        }
+
+        # Make the query and store response in resp
+        resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
