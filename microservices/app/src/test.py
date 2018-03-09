f08ada07d89737e401d48d346e90a033a0cf86f9
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-09 23:44:03 +0530

    first commit

--- microservices/app/src/test.py
+++ microservices/app/src/test.py
@@ -1,38 +1,25 @@
 import json,requests
 from random import randint
 print randint(100000, 999999)
-aadhar='476894302178'
-if len(aadhar) != 12:
-    print "2"
-url = "https://data.despairing12.hasura-app.io/v1/query"
+aadhar='7297899599'
+
+url = "https://notify.despairing12.hasura-app.io/v1/send/sms"
 
 # This is the json payload for the query
 requestPayload = {
-    "type": "select",
-    "args": {
-        "table": "Aadhar",
-        "columns": ["mobile"],
-        "where": {
-            "aadhar_no": {
-                "$eq": aadhar
-            }
-        }
-    }
-}
-
+         "to": aadhar,
+         "countryCode": "91",
+        "message": "OTP For Aadhar verification at Shark@JNU is  "
+          }
 # Setting headers
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer 4f3156a40c12394198aaa87dacd0b53ebf32d1d3ee4271b8"
-}
+headers = {           "Content-Type": "application/json",
+                "Authorization": "Bearer 4f3156a40c12394198aaa87dacd0b53ebf32d1d3ee4271b8"
+            }
 
-# Make the query and store response in resp
+            # Make the query and store response in resp
 resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
-
-if len(resp.json())==0:
+print resp.content
+if 'id' in resp.json():
+    print "ok"
+else:
     print "no"
-try:
-    print resp.json()[0]['mobile']
-except IndexError:
-    print ""
-
