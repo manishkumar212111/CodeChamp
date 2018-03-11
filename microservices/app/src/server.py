f8f0d99d7acebf7d38c3dfd1ce53ad6d662845c1
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-11 14:53:43 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -23,7 +23,7 @@ def encode(key, clear):
         enc.append(enc_c)
     return (base64.urlsafe_b64encode("".join(enc)))
 def id_generator(size=11, chars=string.ascii_uppercase+string.ascii_lowercase + string.digits):
-   return bytes(''.join(random.choice(chars) for _ in range(size)))
+   return ''.join(random.choice(chars) for _ in range(size))
 def email(toaddr, sub, body):
         fromaddr = "t68pf1@gmail.com"
         # toaddr = "manish.kumar212111@gmail.com"
@@ -524,7 +524,7 @@ def DOTTSP():
                 "objects": [
                     {
                         "username": username,
-                        "password": encode(key , p)
+                        "password": encode(app.secret_key , p)
                     }
                 ]
             }
--- microservices/app/src/test.py
+++ microservices/app/src/test.py
@@ -1,5 +1,5 @@
 import base64
-import string,random
+import string,random,requests,json
 def id_generator(size=11, chars=string.ascii_uppercase+string.ascii_lowercase + string.digits):
    return ''.join(random.choice(chars) for _ in range(size))
 def encode(key, clear):
@@ -18,11 +18,35 @@ def decode(key, enc):
         dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
         dec.append(dec_c)
     return "".join(dec)
+p=id_generator()
+key="huheuhr3u4h3urieurw"
+url = "https://data.despairing12.hasura-app.io/v1/query"
 
+# This is the json payload for the query
+requestPayload = {
+    "type": "insert",
+    "args": {
+        "table": "login_ceredential",
+        "objects": [
+            {
+                "username": "sdfshfjsf",
+                "password": encode(key,p)
+            }
+        ]
+    }
+}
 
-print encode('1131whejdwiuedhwe',id_generator())
-print decode('1131whejdwiuedhwe','npKhYqmbmZ8=')
+# Setting headers
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer 4f3156a40c12394198aaa87dacd0b53ebf32d1d3ee4271b8"
+}
 
+# Make the query and store response in resp
+resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+# resp.content contains the json response.
+print resp.content
 
 
 
