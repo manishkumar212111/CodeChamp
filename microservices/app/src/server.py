afcc2ee818784bd35f274100942c95f37c7c290d
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-11 15:21:29 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,7 +1,7 @@
 
 from flask import render_template,Flask,request,url_for,redirect,session
 import requests,json
-import base64
+import bz2
 import smtplib
 import string
 from email.mime.text import MIMEText
@@ -14,15 +14,7 @@ from datetime import datetime
 app=Flask(__name__)
 app.secret_key = "287tdw8d7we6554rrtrgdweyt26etedgdge45"
 #********************************FUNCTION***********************************
-def encode(key, clear):
-    enc = []
-    for i in range(len(clear)):
-        key_c = key[i % len(key)]
-        enc_c = bytes((ord(clear[i]) + ord(key_c)) % 256)
 
-        enc.append(enc_c)
-
-    return (base64.b64encode("".join(enc)))
 def id_generator(size=11, chars=string.ascii_uppercase+string.ascii_lowercase + string.digits):
    return ''.join(random.choice(chars) for _ in range(size))
 def email(toaddr, sub, body):
@@ -44,14 +36,6 @@ def email(toaddr, sub, body):
         server.quit()
         return True
 
-def decode(key, enc):
-    dec = []
-    enc = base64.urlsafe_b64decode(enc)
-    for i in range(len(enc)):
-        key_c = key[i % len(key)]
-        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
-        dec.append(dec_c)
-    return "".join(dec)
 #****************************************************************************
 @app.route('/')
 def home():
@@ -525,7 +509,7 @@ def DOTTSP():
                 "objects": [
                     {
                         "username": username,
-                        "password": encode(app.secret_key , p)
+                        "password": bz2.compress(p)
                     }
                 ]
             }
--- microservices/app/src/test.py
+++ microservices/app/src/test.py
@@ -1,55 +1,5 @@
-import base64
-import string,random,requests,json
-def id_generator(size=11, chars=string.ascii_uppercase+string.ascii_lowercase + string.digits):
-   return ''.join(random.choice(chars) for _ in range(size))
-def encode(key, clear):
-    enc = []
-    for i in range(len(clear)):
-        key_c = key[i % len(key)]
-        enc_c = bytes(chr((ord(clear[i]) + ord(key_c)) % 256))
-        enc.append(enc_c)
+import bz2
+password = "manish"
+encrypted_password = bz2.compress(password)
 
-    return bytes(base64.b64encode("".join(enc)))
-
-def decode(key, enc):
-    dec = []
-    enc = base64.urlsafe_b64decode(enc)
-    for i in range(len(enc)):
-        key_c = key[i % len(key)]
-        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
-        dec.append(dec_c)
-    return "".join(dec)
-p=id_generator()
-key='fsf'
-print encode(key,"bfjbsdj")
-key="huheuhr3u4h3urieurw"
-url = "https://data.despairing12.hasura-app.io/v1/query"
-
-# This is the json payload for the query
-requestPayload = {
-    "type": "insert",
-    "args": {
-        "table": "login_ceredential",
-        "objects": [
-            {
-                "username": "sdfshfjsf",
-                "password": encode(key,p)
-            }
-        ]
-    }
-}
-
-# Setting headers
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer 4f3156a40c12394198aaa87dacd0b53ebf32d1d3ee4271b8"
-}
-
-# Make the query and store response in resp
-#resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
-
-# resp.content contains the json response.
-#print resp.content
-
-
-
+print bz2.decompress(encrypted_password)
