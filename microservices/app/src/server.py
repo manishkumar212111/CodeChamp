75ce871a59edf4f135159350fa39d37cdbc552b0
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-14 22:03:19 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1069,7 +1069,7 @@ def api_consumer_otp():
         # Make the query and store response in resp
         resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
         try:
-            if len(resp.json()[0]):
+            if len(resp.json()[0]['aadhar']):
                 data={
                     "message":"invalid OTP"
                 }
--- microservices/app/src/test.py
+++ microservices/app/src/test.py
@@ -1,11 +1,12 @@
 import requests,json
 
-#url = "http://127.0.0.1:5000/aadhar"
-url="https://app.despairing12.hasura-app.io/api/aadhar_otp"
+url = "http://127.0.0.1:5000/api/consumer_otp"
+#url="https://app.despairing12.hasura-app.io/api/consumer_otp"
 # This is the json payload for the query
 requestPayload = {
     "data":{
-        "aadhar":"875643238312"
+        "OTP":698287,
+        "ID":29
     }
  }
 
