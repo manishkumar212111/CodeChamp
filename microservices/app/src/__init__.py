65b3b2fdf051afb5199a423d2a36baefebd299d6
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-15 21:45:38 +0530

    first commit

--- microservices/app/src/__init__.py
+++ microservices/app/src/__init__.py
@@ -1,14 +1,14 @@
-from flask import Flask
+#from flask import Flask
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 # This line adds the hasura example routes form the hasura.py file.
 # Delete these two lines, and delete the file to remove them from your project
 # from .hasura import hasura_example
 # app.register_blueprint(hasura_examples)
-from tsp import *
+#from tsp import *
 
-from .server import *
-from .data import *
-from .auth import *
-from .filestore import *
+#from .server import *
+#from .data import *
+#from .auth import *
+#from .filestore import *
