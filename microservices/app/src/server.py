1d080f6b72b2e880668993bb87d0c61451aec0c9
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-15 23:20:13 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -639,11 +639,10 @@ def login_DOT():
 # DOT HOME WITH SIM more than 9 and search bar
 @app.route('/DOT/home')
 def DOT_home():
-    try:
-        username=session['DOT_username']
-        return DOT.home(username)
-    except:
-        return render_template('DOT/login.html', message="Login First")
+    username=session['DOT_username']
+    return DOT.home(username)
+
+    return render_template('DOT/login.html', message="Login First")
 
 
 # dot can search
