18d636be4260d0614fa5546eea93959acec35825
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-17 23:09:42 +0530

    first commit

--- microservices/app/src/DOT.py
+++ microservices/app/src/DOT.py
@@ -148,8 +148,37 @@ def search(aadhar):
             return render_template('DOT/home.html',aadhar=aadhar, search="found", count=len(resp.json()),res=resp.json())
 
 
+def mobile_search(mobile):
+    # This is the url to which the query is made
+    url = "https://data.despairing12.hasura-app.io/v1/query"
 
+    # This is the json payload for the query
+    requestPayload = {
+        "type": "select",
+        "args": {
+            "table": "central",
+            "columns": [
+                "aadhar_no"
+            ],
+            "where": {
+                "mobile": {
+                    "$eq": mobile
+                }
+            }
+        }
+    }
 
+    # Setting headers
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer 4f3156a40c12394198aaa87dacd0b53ebf32d1d3ee4271b8"
+    }
+
+    # Make the query and store response in resp
+    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+    return resp.content
+
+#**************************************************ANDROID********************************************************
 def api_home():
 
         url = "https://data.despairing12.hasura-app.io/v1/query"
