f64fcb91bad5a57b2e70e8839e7512646c4b1135
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-23 23:07:45 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -446,11 +446,11 @@ def view_complain():
     resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
     try:
         if len(resp.json()[0])==0:
-            return render_template('support/notification.html',message="No new messages found",value="good")
+            return render_template('support/notification.html',message="No new messages found",value="good1")
         else:
-            return render_template('support/notification.html',result=resp.json()[0],value="Good")
+            return render_template('support/notification.html',result=resp.json()[0],value="Good2")
     except:
-        return render_template('support/notification.html', message="no notification found",value="good")
+        return render_template('support/notification.html', message="no notification found",value="good3")
 
 
 @app.route('/support/logout')
