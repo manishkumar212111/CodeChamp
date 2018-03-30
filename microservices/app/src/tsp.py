66be7fae2bc99e0112a99c4e9c2353a3e0b2feae
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-30 09:22:47 +0530

    first commit

--- microservices/app/src/tsp.py
+++ microservices/app/src/tsp.py
@@ -102,5 +102,5 @@ def search(aadhar):
 
             return render_template('TSP/home.html', response=response,aadhar=aadhar, result=resp.json()['count'])
     except:
-        return render_template('TSP/home.html', aadhar=aadhar, result=resp.json())
+        return render_template('TSP/home.html', aadhar=aadhar, result=resp.json()['count'])
     return render_template('TSP/home.html', aadhar=aadhar, result="Server busy")
