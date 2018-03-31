0ce3a91870bb418e2f0357157c13b6f1b086f095
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-31 12:51:52 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1350,6 +1350,56 @@ def api_aadhar_TSP():
             return jsonify(data=data)
     return "Get method expected"
 
+@app.route('/api/LSA/TSP',methods=['POST','GET'])
+def api_LSA_TSP():
+    if request.method=='POST':
+
+        url = "https://data.despairing12.hasura-app.io/v1/query"
+        # Select aadhar number with their count
+        # This is the json payload for the query
+        requestPayload = {
+            "type": "run_sql",
+            "args": {
+            "sql": "SELECT comp_name ,\"LSA\",COUNT(*) FROM central  GROUP BY comp_name,\"LSA\" ORDER BY aadhar_no"
+            }
+        }
+
+        # Setting headers
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer 5dd53ad731ff1c7dc1c0b74f14052d66699239d69280bf7a"
+        }
+
+        # Make the query and store response in resp
+        resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+        list = []
+        try:
+            if len(resp.json()['result']) == 1:
+                data = {"message": "No result found"}
+                return jsonify(data=data)
+
+            for i in range(1, len(resp.json()['result'])):
+                if int(resp.json()['result'][i][2]) > 9:
+                    list.append([resp.json()['result'][i][0], resp.json()['result'][i][1],resp.json()['result'][i][2]])
+
+            data = {
+
+                "list": list
+
+            }
+            if len(list) == 0:
+                data = {"message": "No result found"}
+                return jsonify(data=data)
+
+            else:
+                return jsonify(data=data)
+        except:
+            data = {"message": "Exception occurred"}
+            return jsonify(data=data)
+    return "Get method expected"
+
+
+
 
 
 
