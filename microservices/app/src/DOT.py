7c088278a73684b61d2dbdf68e914ba9b88fa3f5
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-31 13:24:20 +0530

    first commit

--- microservices/app/src/DOT.py
+++ microservices/app/src/DOT.py
@@ -201,7 +201,7 @@ def api_home():
                 return "0"
 
             for i in range(1, len(resp.json()['result'])):
-                if int(resp.json()['result'][i][1]) > 1:
+                if int(resp.json()['result'][i][1]) > 9:
                     list.append([resp.json()['result'][i][0], resp.json()['result'][i][1]])
             data = {
 
