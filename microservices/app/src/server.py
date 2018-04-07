dd29ffd8e4ec63865cdd4deb0e7c019efaa57f24
manishkumar212111 <manish.kumar212111@gmail.com>
2018-04-08 03:40:31 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -255,8 +255,7 @@ def login_department():
             if resp.json()[0]:
 
                 response=resp.json()[0]['department']
-                if response == 'support':
-                    return redirect(url_for('view_data_entry'))
+
                 url = "https://data.despairing12.hasura-app.io/v1/query"
 
                 # This is the json payload for the query
