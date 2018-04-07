7569a0030c102ce36db5c09106f7348bd3e191a8
manishkumar212111 <manish.kumar212111@gmail.com>
2018-04-08 03:53:27 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,jsonify,session,redirect,url_for
+from flask import flash,Flask,render_template,request,jsonify,session,redirect,url_for
 import requests,json
 import base64
 import datetime
@@ -59,40 +59,45 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/Data/Entry')
+@app.route('/Data/Entry',methods=['POST','GET'])
 def data_entry():
-    url = "https://data.despairing12.hasura-app.io/v1/query"
+    if request.method=='POST':
+        password=request.form['password']
+        if password!='7297':
+            return render_template('index.html',value="Incorrect password")
 
-    # This is the json payload for the query
-    requestPayload = {
-        "type": "select",
-        "args": {
-            "table": "problem_dummy",
-            "columns": [
-                "p_id",
-                "latitude",
-                "longitude",
-                "im_id",
-                "date"
-            ]
+        url = "https://data.despairing12.hasura-app.io/v1/query"
+
+        # This is the json payload for the query
+        requestPayload = {
+            "type": "select",
+            "args": {
+                "table": "problem_dummy",
+                "columns": [
+                    "p_id",
+                    "latitude",
+                    "longitude",
+                    "im_id",
+                    "date"
+                ]
+            }
         }
-    }
 
-    # Setting headers
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer 8cafc32cc39fe0e17b06bd326a2cfbfbf968110117f29767"
-    }
+        # Setting headers
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer 8cafc32cc39fe0e17b06bd326a2cfbfbf968110117f29767"
+        }
 
-    # Make the query and store response in resp
-    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+        # Make the query and store response in resp
+        resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
 
-    return render_template('DbEntry.html',res=resp.json())
+        return render_template('DbEntry.html',res=resp.json())
 
 def getlocation(lati,lon):
     return "Sardar Vallabhbhai Engineering College Rd, SVNIT Campus, Athwa, Surat, Gujarat 395007, India"
 
-
+@app.route('/view/Data/Entry',methods=['POST','GET'])
 def view_data_entry():
     p_id=request.args.get('p_id')
     url = "https://data.despairing12.hasura-app.io/v1/query"
@@ -255,7 +260,8 @@ def login_department():
             if resp.json()[0]:
 
                 response=resp.json()[0]['department']
-
+                if response=='support':
+                    return redirect(url_for(''))
                 url = "https://data.despairing12.hasura-app.io/v1/query"
 
                 # This is the json payload for the query
