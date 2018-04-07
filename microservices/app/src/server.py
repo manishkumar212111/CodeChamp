7f07b764881804eaa0c8acc8a540415671cf7fe3
manishkumar212111 <manish.kumar212111@gmail.com>
2018-04-08 04:14:50 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -207,7 +207,33 @@ def submit_data():
                 resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
                 try:
                     if resp.json()['affected_rows']:
-                        return render_template('DbEntry.html',value="Value Successfully entered")
+                        url = "https://data.despairing12.hasura-app.io/v1/query"
+
+                        # This is the json payload for the query
+                        requestPayload = {
+                            "type": "select",
+                            "args": {
+                                "table": "problem_dummy",
+                                "columns": [
+                                    "p_id",
+                                    "latitude",
+                                    "longitude",
+                                    "im_id",
+                                    "date"
+                                ]
+                            }
+                        }
+
+                        # Setting headers
+                        headers = {
+                            "Content-Type": "application/json",
+                            "Authorization": "Bearer 8cafc32cc39fe0e17b06bd326a2cfbfbf968110117f29767"
+                        }
+
+                        # Make the query and store response in resp
+                        resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+                        return render_template('DbEntry.html', res=resp.json(),value="Value Successfully entered")
                 except:
                     return render_template('DbEntry.html', value="Exception occurred1")
 
