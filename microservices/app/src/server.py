00e2dba001730fcc85bdd919b6fdf33fb91c81c1
manishkumar212111 <manish.kumar212111@gmail.com>
2018-04-08 00:05:02 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -133,12 +133,14 @@ def submit_data():
 
         # Setting headers
         headers = {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": "Bearer 8cafc32cc39fe0e17b06bd326a2cfbfbf968110117f29767"
+
         }
 
         # Make the query and store response in resp
         resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
-
+        return resp.content
 
 @app.route('/login/department',methods=['POST','GET'])
 def login_department():
