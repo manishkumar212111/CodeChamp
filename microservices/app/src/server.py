1c1a0344fc555ecad38fe3e4204ee42fb24eb1d4
manishkumar212111 <manish.kumar212111@gmail.com>
2018-04-07 17:48:45 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request,jsonify
 import requests,json
+import base64
 
 app=Flask(__name__)
 
@@ -75,8 +76,45 @@ def image_upload():
     if request.method=='POST':
         content = request.get_json(force=True)
         js = json.loads(json.dumps(content))
+        decoded = base64.b64decode(js['data']['image'])
+        url = "https://data.despairing12.hasura-app.io/v1/query"
 
-        return jsonify(data=js['data']['image'])
+        # This is the json payload for the query
+        requestPayload = {
+            "type": "insert",
+            "args": {
+                "table": "test",
+                "objects": [
+                    {
+                        "Name": js['data']['image'],
+                        "ID": "222"
+                    }
+                ]
+            }
+        }
+
+        # Setting headers
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer 8cafc32cc39fe0e17b06bd326a2cfbfbf968110117f29767"
+        }
+
+        # Make the query and store response in resp
+        resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+        url = "https://filestore.octagon58.hasura-app.io/v1/decoded/"
+
+        # Setting headers
+        headers = {
+            "Content-Type": "image / png",
+            "Authorization": "Bearer bbaa14a4678aa95b48f009258441ea4dd383b90231cbb544"
+        }
+
+        # Open the file and make the query
+        # with open(file.filename, 'rb') as file_image:
+        resp = requests.put(url, data=file, headers=headers)
+
+        return resp.content
     return "POST method expected"
 
 
