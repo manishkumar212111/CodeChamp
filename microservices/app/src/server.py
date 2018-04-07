ae444b2b54a42f138805b70bed727035f72b08da
manishkumar212111 <manish.kumar212111@gmail.com>
2018-04-07 23:04:55 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -2,13 +2,13 @@ from flask import Flask,render_template,request,jsonify,session
 import requests,json
 import base64
 import datetime
-from geopy.geocoders import GoogleV3
+#from geopy.geocoders import GoogleV3
 app=Flask(__name__)
 
 app.secret_key = "287tdw8d7we6554rrtrgdweyt26etedgdge45"
 
 def getLocation(lati,longi):
-    geocoder = GoogleV3()
+    geocoder = "a"
     location_list = geocoder.reverse((21.1673500, 72.7850900))
     location = location_list[0]
     address = location.address
