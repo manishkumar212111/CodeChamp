7205df4e71759c42437281d808fbc1bf3be95c61
manishkumar212111 <manish.kumar212111@gmail.com>
2018-04-07 15:57:44 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,8 +1,65 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import requests,json
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login/department',methods=['POST','GET'])
+def login_department():
+    if request.method=='POST':
+        email=request.form['email']
+        password=request.form['password']
+
+        url = "https://data.despairing12.hasura-app.io/v1/query"
+
+        # This is the json payload for the query
+        requestPayload = {
+            "type": "select",
+            "args": {
+                "table": "user_table",
+                "columns": [
+                    "department"
+                ],
+                "where": {
+                    "$and": [
+                        {
+                            "email": {
+                                "$eq": email
+                            }
+                        },
+                        {
+                            "password": {
+                                "$eq": password
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+
+        # Setting headers
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer 8cafc32cc39fe0e17b06bd326a2cfbfbf968110117f29767"
+        }
+
+        # Make the query and store response in resp
+        resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+        try:
+            if resp.json()['department']:
+                return "Success"
+            else:
+                return "INcorrect credential"
+        except:
+            return resp.json()
+    return "Get method expected"
+
+
+if __name__:
+    app.run(debug=True)
+
 
