dc205853ab3a47e7c80034fa1598f5d4aab59893
manishkumar212111 <manish.kumar212111@gmail.com>
2018-04-08 01:57:10 +0530

    first commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -17,7 +17,42 @@ def getLocation(lati,longi):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    url = "https://data.despairing12.hasura-app.io/v1/query"
+
+    # This is the json payload for the query
+    requestPayload = {
+        "type": "select",
+        "args": {
+            "table": "problem_db",
+            "columns": [
+                "p_id",
+                "p_st",
+                "p_category",
+                "sub_date",
+                "sol_date",
+                "address",
+                "status"
+            ],
+            "order_by": [
+                {
+                    "column": "status",
+                    "order": "asc"
+                }
+            ]
+        }
+    }
+
+    # Setting headers
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer 8cafc32cc39fe0e17b06bd326a2cfbfbf968110117f29767"
+    }
+
+    # Make the query and store response in resp
+    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+
+    return render_template('index.html',res=resp.json())
 
 @app.route('/login',methods=['POST','GET'])
 def login():
